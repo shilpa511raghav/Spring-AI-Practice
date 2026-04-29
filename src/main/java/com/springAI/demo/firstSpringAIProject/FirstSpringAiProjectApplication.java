64ff52152335d54ff01e5dd285fd182371a5e242
shilpa <shilpa511raghav@gmail.com>
2026-04-29 23:41:54 +0530

    initial commit

---
+++ src/main/java/com/springAI/demo/firstSpringAIProject/FirstSpringAiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.springAI.demo.firstSpringAIProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springAI/demo/firstSpringAIProject/FirstSpringAiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springAI.demo.firstSpringAIProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringAiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
